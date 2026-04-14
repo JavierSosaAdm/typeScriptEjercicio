7c1837745172964590dd8d67bf3fd18b5974becd
JavierSosaAdm <javier.sosa.1911@gmail.com>
2026-04-14 12:53:52 -0300

    se agrega funcionalidad de teclado a los botones agregar y borrar

--- src/components/TodoApp.tsx
+++ src/components/TodoApp.tsx
@@ -14,13 +14,21 @@ export const TodoApp = () => {
         setListaTareas(tareas => tareas.filter((_,i) => i !== index))
     }
 
+    const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === 'Enter') {
+            handleAddTask();
+        }
+    }
+
     return (
         <div>
             <h1>Lista de tareas</h1>
             <div>
-                <input type="text"
+                <input 
+                    type="text"
                     value={nuevaTarea}
                     onChange={(e) => setNuevaTarea(e.target.value)}
+                    onKeyDown={handleKeyDown}
                     placeholder="Nueva tarea" />
             <button onClick={handleAddTask}>Agregar</button>
             </div>
--- src/components/tarea.tsx
+++ src/components/tarea.tsx
@@ -1,14 +1,36 @@
+import { useEffect, useRef } from "react";
+
 type TareaProps = {
     tarea: string,
     borrarTarea: () => void
 }
 
 export const Tarea = ({tarea, borrarTarea}: TareaProps) => {
+    const divRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        const handleKeyDown = (e:KeyboardEvent) => {
+            if (e.key === 'Delete') {
+                borrarTarea();
+            }
+        };
+
+        const current = divRef.current;
+        current?.addEventListener('keydown', handleKeyDown);
+
+        return () => {
+            current?.removeEventListener('keydown', handleKeyDown);
+        };
+    }, [borrarTarea])
     return (
-        <div className="task">
+        <div
+            ref={divRef}
+            tabIndex={0}
+            className="task"
+        >
             <span>{tarea}</span>
             <button onClick={borrarTarea}>Borrar</button>
         </div>
     )
     
-}
+};
